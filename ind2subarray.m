b2ee15701f72a842151d6985b3b49543c1da5c4e
Lucas Jeub <ljeub@iu.edu>
2016-10-10 12:31:10 -0400

    rename some variables for clarity

--- ind2subarray.m
+++ ind2subarray.m
@@ -1,22 +1,23 @@
-function [sub] = ind2subarray(siz,ndx)
+function [sub] = ind2subarray(shape,ind)
 %IND2SUBARRAY Subscripts from linear index. Same as IND2SUB but
 %   returns results as an array instead of separate output arguments.
 
-siz = double(siz);
-lensiz = length(siz);
-sub=zeros(length(ndx),length(siz));
 
-k = cumprod(siz);
-if ndx>k(end)
+shape = double(shape);
+dims = length(shape);
+sub=zeros(length(ind),length(shape));
+
+k = cumprod(shape);
+if ind>k(end)
     error('MultilayerBenchmark:ind2subarray:IndexOutOfRange','Index out of range.');
 end
-for i = lensiz:-1:2,
-    vi = rem(ndx-1, k(i-1)) + 1;
-    vj = (ndx - vi)/k(i-1) + 1;
-    sub(:,i) = double(vj);
-    ndx = vi;
+for i = dims:-1:2,
+    subi = rem(ind-1, k(i-1)) + 1;
+    subj = (ind - subi)/k(i-1) + 1;
+    sub(:,i) = double(subj);
+    ind = subi;
 end
-sub(:,1)=ndx;
+sub(:,1)=ind;
 
 end
 
--- subarray2ind.m
+++ subarray2ind.m
@@ -1,24 +1,24 @@
-function ndx = subarray2ind(siz,suba)
+function ndx = subarray2ind(shape,suba)
 %SUBARRAY2IND Linear index from multiple subscripts.
 %   Same as SUB2IND but takes array as input rather than multiple arguments.
-siz = double(siz);
-lensiz = length(siz);
-if size(suba,2)~=lensiz
+shape = double(shape);
+dims = length(shape);
+if size(suba,2)~=dims
    error('MultilayerBenchmark:subarray2ind:DimensionMismatch','The subscript dimension does not match the array size.');
 end
 
 
-if any(min(suba(:,1)) < 1) || any(max(suba(:,1)) > siz(1))
+if any(min(suba(:,1)) < 1) || any(max(suba(:,1)) > shape(1))
     %Verify subscripts are within range
     error('MultilayerBenchmark:subarray2ind:IndexOutOfRange','Subscript out of range.');
 end
 
 ndx = double(suba(:,1));
     %Compute linear indices
-    k = cumprod(siz);
-    for i = 2:lensiz
+    k = cumprod(shape);
+    for i = 2:dims
         %%Input checking
-        if (any(min(suba(:,i)) < 1)) || (any(max(suba(:,i)) > siz(i)))
+        if (any(min(suba(:,i)) < 1)) || (any(max(suba(:,i)) > shape(i)))
             %Verify subscripts are within range
             error('MultilayerBenchmark:subarray2ind:IndexOutOfRange','Subscript out of range.');
         end
